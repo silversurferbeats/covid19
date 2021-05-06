9a16de72fa2e9f0409fe904dda1b6411aa7d3eb5
SilversurferbeatS <gabrielsuarez89@gmail.com>
2021-05-06 00:59:39 -0300

    Updates

---
+++ precache-manifest.10b817220c41011e0d93ce2675db5e78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b661c2e0b087781c46f17d734e9c638e",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "03ede49a6e6748d05785",
+    "url": "/covid19/static/css/main.d2526a50.chunk.css"
+  },
+  {
+    "revision": "1a8d53511e910e22aa8f",
+    "url": "/covid19/static/js/2.e57ea296.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.e57ea296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ede49a6e6748d05785",
+    "url": "/covid19/static/js/main.dc236035.chunk.js"
+  },
+  {
+    "revision": "384fdf37c74f6a26a4c0",
+    "url": "/covid19/static/js/runtime-main.2993b3e3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "0e8bf5657091c091a1d027d176c827bf",
+    "url": "/covid19/static/media/imgBG2.0e8bf565.png"
+  }
+]);
